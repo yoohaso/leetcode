2460b36b0476a3978765fde96d480a11a4b0a0b6
YUN HANSOL <46476678+yoohaso@users.noreply.github.com>
2025-03-04 14:02:19 +0900

    Time: 7 ms (6.42%), Space: 58.1 MB (5.28%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.ts
+++ 0290-word-pattern/0290-word-pattern.ts
@@ -1,17 +1,18 @@
 function wordPattern(pattern: string, s: string): boolean {
     const words = s.split(' ');
-    const patternToWord = {};
-    const wordToPattern = {};
+    const patternToWord = new Map();
+    const wordToPattern = new Map();
 
     if (pattern.length !== words.length) {
         return false;
     }
 
     for (let i = 0; i < pattern.length; i++) {
-        if (!patternToWord[pattern[i]] && !wordToPattern[words[i]]) {
-            patternToWord[pattern[i]] = words[i];
-            wordToPattern[words[i]] = pattern[i];
-        } else if (patternToWord[pattern[i]] !== words[i]) {
+        console.log('word', words[i], wordToPattern[words[i]]);
+        if (!patternToWord.has(pattern[i]) && !wordToPattern.has(words[i])) {
+            patternToWord.set(pattern[i], words[i]);
+            wordToPattern.set(words[i], pattern[i]);
+        } else if (patternToWord.get(pattern[i]) !== words[i]) {
             return false;
         }
     }
