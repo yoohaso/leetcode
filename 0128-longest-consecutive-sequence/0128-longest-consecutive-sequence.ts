13a2485dc02f87e9bfb94986c5ebab769f3a2f41
YUN HANSOL <46476678+yoohaso@users.noreply.github.com>
2025-07-31 23:52:34 +0900

    Time: 61 ms (16.41%), Space: 81.1 MB (67.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -1,18 +1,19 @@
 function longestConsecutive(nums: number[]): number {
+    nums.sort((a, b) => a - b);
     const numsSet = new Set(nums);
+    let count = 1;
     let result = 0;
 
+    if (numsSet.size === 1) {
+        return 1;
+    }
+    
     for (const num of numsSet) {
-        if (!numsSet.has(num - 1)) {
-            let currentNum = num;
-            let count = 0;
-
-            while (numsSet.has(currentNum)) {
-                currentNum++;
-                count++;
-            }
-
-            result = Math.max(result, count);
+        if (!numsSet.has(num + 1)) {
+            result = Math.max(count, result);
+            count = 1;
+        } else {
+            count++;
         }
     }
 
