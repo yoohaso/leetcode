1d756cd5ba23b021818d3c7803ecc3654e386550
YUN HANSOL <46476678+yoohaso@users.noreply.github.com>
2025-08-11 11:33:56 +0900

    Time: 41 ms (75.78%), Space: 53.9 MB (86.21%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.ts
@@ -0,0 +1,29 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    const expectedVal = val;
+    return {
+        toBe: (val) => {
+            if (expectedVal === val) {
+                return true;
+            }
+
+            throw 'Not Equal';
+        },
+        notToBe: (val) => {
+            if (expectedVal !== val) {
+                return true;
+            }
+
+            throw 'Equal';
+        },
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
