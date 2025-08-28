fe5fe47ee1bc69f789076788a7724a2bcc574d81
YUN HANSOL <46476678+yoohaso@users.noreply.github.com>
2025-08-28 17:25:36 +0900

    Time: 1 ms (86.87%), Space: 63.7 MB (95.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -2,7 +2,7 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    const step = nums.length > k ? k : k % nums.length;
-    const toMoveFowardNums = nums.splice(nums.length - step, step)
-    nums.unshift(...toMoveFowardNums);
+    const step = k % nums.length;
+    const toRotateNums = nums.splice(nums.length - step, step);
+    nums.unshift(...toRotateNums);
 };
